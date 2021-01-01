080bce13f91a601db8f6a10e74ceb6b84b9ab7b7
Andrea Colledan <and.colledan@gmail.com>
2021-01-01 18:59:24 +0100

    Deleted Language.Interface (useless)

--- app/Main.hs
+++ app/Main.hs
@@ -9,7 +9,6 @@ import Circuit.Gate
 import Circuit.Dynamic.Class
 import Circuit.Dynamic.Naive
 
-import Language.Interface
 import Language.Core as Core
 import Language.Lift as Lift
 import Language.Box as Box
--- src/Language/Interface.hs
+++
@@ -1,9 +0,0 @@
-module Language.Interface where
-
-import Interface
-
-import Language.Core
-import Language.Lift
-import Language.Box
-
-type PQM exp = (HasLolli exp, HasTensor exp, HasCore exp, HasLift exp, HasBox exp)
