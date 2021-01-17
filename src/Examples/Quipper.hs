dec2f50f5de42cc2e553f54a471b74c4436649a1
Andrea Colledan <and.colledan@gmail.com>
2021-01-17 12:35:34 +0100

    Implemented quantum teleportation

--- src/Examples/Quipper.hs
+++ src/Examples/Quipper.hs
@@ -8,6 +8,8 @@
 
 module Examples.Quipper where
 
+import Prelude hiding ((^))
+
 import LNLHask
 import Circuit.Gate
 import Circuit.Dynamic.Class
@@ -29,17 +31,41 @@ plusMinus b = apply (circuit (VLabel 0) (fromGate H) (VLabel 1)) input where
         True -> (apply (circuit (VLabel 0) (fromGate X) (VLabel 1))) (label l0)
         False -> label l0
 
-share :: (KnownNat x, KnownNat (x+1),
-    CMerge '[ '(x,Qubit) ] '[ '(x+1,Qubit) ] '[ '(x,Qubit), '(x+1,Qubit) ])
-    => Deep '[ '(x,Qubit) ] Qubit -> Deep '[ '(x,Qubit), '(x+1,Qubit) ] (Qubit ⊗ Qubit) --TODO this should be more general!
-share (exp :: Deep '[ '(x,Qubit) ] Qubit) = apply
-                                            (circuit (VLabel 0 `VPair` VLabel 1) (fromGate (C X)) (VLabel 2 `VPair` VLabel 3))
-                                            (exp ⊗ label (Proxy :: Proxy (x+1)))
+share :: Deep '[ '(1,Qubit) ] (Qubit ⊸ (Qubit ⊗ Qubit))
+share = λ $ \a -> apply
+            (circuit (VLabel 0 `VPair` VLabel 1) (fromGate (C X)) (VLabel 2 `VPair` VLabel 3))
+            (a ⊗ label (l1))
 
 bell00 :: Deep '[ '(0,Qubit),'(1,Qubit) ] (Qubit ⊗ Qubit)
-bell00 = let a = plusMinus False in share a
+bell00 = share ^ (plusMinus False)
+
+--Quantum teleportation
+
+alice :: Deep '[] (Qubit ⊸ Qubit ⊸ (Qubit ⊗ Qubit)) --for now we do not have bits
+-- alice = λq. λa. let ⟨q',a'⟩ = apply((⟨0,1⟩,CX,⟨2,3⟩),⟨q,a⟩) in apply((⟨0,1⟩,Meas2,⟨2,3⟩),⟨apply((0,H,1),q'),a'⟩)
+alice = λ $ \q -> λ $ \a ->
+        apply (circuit (VLabel 0 `VPair` VLabel 1) (fromGate $ C X) (VLabel 2 `VPair` VLabel 3)) (q ⊗ a)
+        `letPair` \(q',a') -> apply (circuit (VLabel 0 `VPair` VLabel 1) (measure 2) (VLabel 2 `VPair` VLabel 3))
+        (apply (circuit (VLabel 0) (hadamard 1) (VLabel 1)) q' ⊗ a')
 
 
+bob :: Deep '[] (Qubit ⊸ (Qubit ⊗ Qubit) ⊸ (Qubit ⊗ (Qubit ⊗ Qubit)))
+-- bob = λb. λxy. let ⟨x,y⟩ = xy in
+--            let ⟨y',b'⟩ = apply ((⟨0,1⟩,CX,⟨2,3⟩),⟨y,b⟩) in
+--            let ⟨x',b''⟩ = apply ((⟨0,1⟩,CX,⟨2,3⟩),⟨x,b'⟩) in
+--            ⟨b'',⟨x',y'⟩⟩
+bob = λ $ \b -> λ $ \xy ->
+      xy `letPair` \(x,y) -> (apply (circuit (VLabel 0 `VPair` VLabel 1) (fromGate $ C X) (VLabel 2 `VPair` VLabel 3)) (y ⊗ b))
+      `letPair` \(y',b') -> (apply (circuit (VLabel 0 `VPair` VLabel 1) (fromGate $ C Z) (VLabel 2 `VPair` VLabel 3)) (x ⊗ b'))
+      `letPair` \(x',b'') -> b'' ⊗ (x' ⊗ y')
+
+teleport :: Deep '[ '(0,Qubit),'(1,Qubit) ] (Qubit ⊸ (Qubit ⊗ (Qubit ⊗ Qubit)))
+-- teleport = λq. let ⟨a,b⟩ = bell00 in
+--                let ⟨x,y⟩ = alice q a in
+--                bob b ⟨x,y⟩                 
+teleport = λ $ \q ->
+            bell00 `letPair` \(a,b) -> alice ^ q ^ a
+            `letPair` \(x,y) -> bob ^ b ^ (x ⊗ y)
 
 
 generate :: KnownCtx γ => Deep γ t -> Circuit --Evaluates an expression and runs the resulting circuit-building computation
@@ -49,4 +75,4 @@ generate (exp :: Deep γ t) = c
     (_,c) = runState res (identity $ exp2LabelContext exp)
 
 test :: IO ()
-test = print $ generate bell00
+test = print $ generate (teleport ^ (label l3))
